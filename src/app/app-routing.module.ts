dcefdf7a70bdfdc1eeb7ea17133aa25d52964fe9
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-05 11:22:34 -0300

    tg-31 Crear Project.ts como modelo de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { HomeComponent } from './features/home/home/home.component';
 import { LayoutComponent } from './core/layout/layout.component';
-import { MyProjectsComponent } from './features/projects/my-projects/my-projects.component';
+import { MyProjectsComponent } from './features/project/my-projects/my-projects.component';
 
 const routes: Routes = [
   {
---
+++ src/models/project/project.ts
@@ -0,0 +1,6 @@
+export interface Project {
+  name: string;
+  members: string[]; //Subject to change when user interface is defined
+  description?: string;
+  icon?: string;
+}
