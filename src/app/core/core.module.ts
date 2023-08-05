84d7644716ef82e529abfe29f15fd288a11a915b
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-04 21:11:30 -0300

    tg-20 Generar componente de menu lateral con espacio para encabezado,navegacion y link hacia settings

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, MenuComponent],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
