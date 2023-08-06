99713c34005a771cfda4ee9b030e35f437c580d7
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-05 22:12:15 -0300

    tg-38 Si no existen epics, debe informarse esto a la persona usuaria

--- src/app/features/project/services/epic.service.ts
+++ src/app/features/project/services/epic.service.ts
@@ -5,21 +5,23 @@ import { Epic } from 'src/models/project/epic';
   providedIn: 'root',
 })
 export class EpicService {
-  epicList: Epic[] = [
-    {
-      project: 'PROYA',
-      name: 'epic2',
-      description: 'epic2d esc',
-      icon: 'nose',
-    },
-    {
-      project: 'PROYA',
-      name: 'epic1',
-      description: 'epic1d esc',
-      icon: 'nose',
-    },
-  ];
-  constructor() {}
+  epicList: Epic[] = [];
+  constructor() {
+    this.epicList = [
+      {
+        project: 'PROYA',
+        name: 'epic2',
+        description: 'epic2d esc',
+        icon: 'nose',
+      },
+      {
+        project: 'PROYA',
+        name: 'epic1',
+        description: 'epic1d esc',
+        icon: 'nose',
+      },
+    ];
+  }
 
   getEpics() {
     return this.epicList;
