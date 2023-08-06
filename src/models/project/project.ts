538695a98c8270ba477c50a5822b4adf1a40ca81
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-06 20:07:30 -0300

    tg-45 Completar interfaz Project, agregar atributos respuesta de API

--- src/models/project/project.ts
+++ src/models/project/project.ts
@@ -2,6 +2,8 @@ export interface Project {
   _id: string;
   name: string;
   members: string[]; //Subject to change when user interface is defined
+  owner: string[];
   description?: string;
   icon?: string;
+  __v?: number;
 }
