1d60967b55ffa24f70cfe57727ef64ac18bdec10
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-05 21:39:55 -0300

    tg-41 Crear servicio EpicService. tg-30 Generar componente ProjectDetails

--- src/app/features/project/project.module.ts
+++ src/app/features/project/project.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [MyProjectsComponent],
+  declarations: [MyProjectsComponent, ProjectDetailsComponent],
   imports: [CommonModule],
 })
 export class ProjectModule {}
