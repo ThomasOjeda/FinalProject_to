2cbd78380dae3eeac7789cf3b74f6af55e546b5c
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-07 23:37:35 -0300

    tg-52 Se deben mostrar los detalles del proyecto

--- src/app/features/project/project-details/project-details.component.ts
+++ src/app/features/project/project-details/project-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/models/project/project';
 import { Epic } from 'src/models/project/epic';
 import { EpicService } from '../../epic/services/epic.service';
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-project-details',
@@ -9,11 +11,22 @@ import { EpicService } from '../../epic/services/epic.service';
   styleUrls: ['./project-details.component.scss'],
 })
 export class ProjectDetailsComponent implements OnInit {
-  project!: Project;
+  project: Project | undefined;
   epicList: Epic[] = [];
-  constructor(private epicService: EpicService) {}
+  constructor(
+    private projectService: ProjectService,
+    private epicService: EpicService,
+    private activatedRouteService: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.epicList = this.epicService.getEpics();
+    let projectId =
+      this.activatedRouteService.snapshot.paramMap.get('project-id');
+    if (projectId)
+      this.projectService
+        .getProject(projectId)
+        .subscribe((project) => (this.project = project))
+        .unsubscribe();
   }
 }
