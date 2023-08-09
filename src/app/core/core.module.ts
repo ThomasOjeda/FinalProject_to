f5838ecba84bcba38cf7d7e61c4c43cfbf2383ff
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-09 20:38:36 -0300

    tg-79 Usar botones custom para menu y encabezado

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent, MenuComponent, LayoutComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
