bffe4a78ff9c9c0beb94ad12ee09f5764b78b1d0
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-11 14:01:47 -0300

    tg-88 Crear componente Login

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,9 +5,10 @@ import { MenuComponent } from './menu/menu.component';
 import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [HeaderComponent, MenuComponent, LayoutComponent],
+  declarations: [HeaderComponent, MenuComponent, LayoutComponent, LoginComponent],
   imports: [CommonModule, RouterModule, SharedModule],
   exports: [HeaderComponent],
 })
