ea779ee75ddf17f9d26be74c32f74f8626f726c1
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-14 00:24:21 -0300

    tg-125 Boton custom con distintas alturas

--- src/app/shared/custom-button/custom-button.component.ts
+++ src/app/shared/custom-button/custom-button.component.ts
@@ -1,16 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.scss'],
 })
-export class CustomButtonComponent {
+export class CustomButtonComponent implements OnInit {
   @Output() clickedButtonEvent = new EventEmitter();
   @Input() text: string = '';
   @Input() buttonPaint: string = '';
+  @Input() buttonHeight: string = '';
 
   constructor() {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   buttonWasClicked($event: Event) {
     this.clickedButtonEvent.emit($event);
   }
