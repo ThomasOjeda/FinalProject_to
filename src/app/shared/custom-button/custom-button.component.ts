1764e22e1388b71fce50289f0c61486684012892
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-14 00:07:56 -0300

    tg-123 Estilizar boton custom

--- src/app/shared/custom-button/custom-button.component.ts
+++ src/app/shared/custom-button/custom-button.component.ts
@@ -1,49 +1,17 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnDestroy,
-  OnInit,
-  Output,
-} from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-/* import { ThemeService } from 'src/app/services/theme.service';
- */
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
 @Component({
   selector: 'app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.scss'],
 })
-export class CustomButtonComponent implements OnInit, OnDestroy {
+export class CustomButtonComponent {
   @Output() clickedButtonEvent = new EventEmitter();
-  @Input() iconPath: string = '';
-  @Input() iconAlt: string = '';
   @Input() text: string = '';
-  @Input() buttonType: string = '';
+  @Input() buttonPaint: string = '';
 
-  displayText = false;
-  displayIcon = false;
-
-  theme!: string;
-
-  theme$: Observable<string> = new Observable<string>();
-  themeSubscription: Subscription = new Subscription();
-  constructor(/* private ts: ThemeService */) {}
-
-  ngOnInit(): void {
-    if (this.buttonType == 'text' || this.buttonType == 'text&icon')
-      this.displayText = true;
-    if (this.buttonType == 'icon' || this.buttonType == 'text&icon')
-      this.displayIcon = true;
-
-    /*     this.theme$ = this.ts.getTheme$();
-     */ this.themeSubscription = this.theme$.subscribe((t) => (this.theme = t));
-  }
+  constructor() {}
   buttonWasClicked($event: Event) {
     this.clickedButtonEvent.emit($event);
-  }
-
-  ngOnDestroy() {
-    this.themeSubscription.unsubscribe();
   }
 }
