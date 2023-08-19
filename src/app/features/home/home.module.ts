89f081c11c38e7621997fc69059a6fe2d8179f78
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-19 18:55:26 -0300

    tg-161 usar summary cards para los elementos

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, SharedModule],
 })
 export class HomeModule {}
