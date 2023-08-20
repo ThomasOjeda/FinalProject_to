d20a5e319852ad13bdbbc1aa17e6ac8e86907148
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-20 19:17:32 -0300

    tg-169 arreglar inputs con darkmode

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { TokenService } from '../services/token.service';
 import { Router } from '@angular/router';
+import { ThemeService } from 'src/app/core/services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
   loginUsername: string = '';
   loginPassword: string = '';
   isLoggingIn: boolean = false;
   alertOpen: boolean = false;
+
+  theme: string = '';
+  themeSubscription: Subscription = new Subscription();
   constructor(
     private loginService: LoginService,
     private tokenService: TokenService,
-    private routerService: Router
+    private routerService: Router,
+    private themeService: ThemeService
   ) {}
+
+  ngOnInit(): void {
+    this.themeSubscription = this.themeService
+      .getTheme$()
+      .subscribe((theme) => (this.theme = theme));
+  }
   submit() {
     this.isLoggingIn = true;
     this.loginService.login(this.loginUsername, this.loginPassword).subscribe({
@@ -44,5 +56,8 @@ export class LoginComponent {
 
   handleAlertClosed() {
     this.alertOpen = false;
+  }
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
   }
 }
