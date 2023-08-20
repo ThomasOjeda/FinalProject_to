d55fee388a2059d9d9341131c5a429095207810b
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-19 23:14:28 -0300

    tg-164 crear lista de stories, agregar estilos iguales a las demas listas

--- src/app/features/story/my-stories/my-stories.component.ts
+++ src/app/features/story/my-stories/my-stories.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Story } from 'src/models/story';
+import { StoryService } from '../services/story.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-stories',
   templateUrl: './my-stories.component.html',
   styleUrls: ['./my-stories.component.scss']
 })
-export class MyStoriesComponent {
+export class MyStoriesComponent implements OnInit {
+  storyList: Story[] = [];
+  storyListSubscription: Subscription = new Subscription();
+  loadingStories: boolean = true;
+  errorFetchingStories: boolean = false;
+  constructor(
+    private storyService: StoryService,
+
+  ) {}
+  ngOnInit(): void {
+    this.storyService.getStories$().subscribe({
+      next: (stories) => {
+        this.storyList = stories.data;
+      },
+      error: (err) => {
+        this.loadingStories = false;
+        this.errorFetchingStories = true;
+      },
+      complete: () => {
+        this.loadingStories = false;
+      },
+    });
+  }
 
 }
