ad2493c7bb9075e15ec33a121b65e627dad38b3a
ThomasOjeda <thomasojeda05@gmail.com>
2023-08-29 23:03:30 -0300

    tg-211 Board expandible con animacion

--- src/app/features/epic/epic-data/board/board.component.ts
+++ src/app/features/epic/epic-data/board/board.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Story } from 'src/models/story';
 
@@ -7,18 +13,36 @@ import { Story } from 'src/models/story';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
-export class BoardComponent {
+export class BoardComponent implements AfterViewInit {
+  @ViewChild('storyList') storyListElement!: ElementRef;
+
   @Input() title: string = '';
   @Input() stories: Story[] = [];
+  open: boolean = true;
 
   constructor(
     private routerService: Router,
     private activatedRouteService: ActivatedRoute
   ) {}
 
+  ngAfterViewInit(): void {
+    this.storyListElement.nativeElement.style.maxHeight =
+      this.storyListElement.nativeElement.scrollHeight + 'px';
+  }
+
   handleStorySelection(storyId: string) {
     this.routerService.navigate([storyId], {
       relativeTo: this.activatedRouteService,
     });
+  }
+
+  toggleBoard() {
+    if (this.open) {
+      this.storyListElement.nativeElement.style.maxHeight = '0px';
+    } else {
+      this.storyListElement.nativeElement.style.maxHeight =
+        this.storyListElement.nativeElement.scrollHeight + 'px';
+    }
+    this.open = !this.open;
   }
 }
