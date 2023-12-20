6a4143f4e5c89c024e2c046dd9825da62571c92b
Thomas Ojeda <thomas.ojeda@globant.com>
2023-12-20 10:25:56 -0300

    fixed menu

--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MenuService } from '../services/menu.service';
-import { Router } from '@angular/router';
 import { LoginService } from 'src/app/auth/services/login.service';
 
 @Component({
@@ -14,16 +13,15 @@ export class MenuComponent implements OnInit, OnDestroy {
   menuStateSubscription: Subscription = new Subscription();
 
   mainElements = [
-    { url: '/', name: 'Home', selected: false },
-    { url: 'my-projects', name: 'My Projects', selected: false },
-    { url: '/my-stories', name: 'My Stories', selected: false },
+    { url: '/home', name: 'Home' },
+    { url: 'my-projects', name: 'My Projects' },
+    { url: '/my-stories', name: 'My Stories' },
   ];
 
   footerElements = [{ url: '/settings', name: 'Settings', selected: false }];
 
   constructor(
     private menuService: MenuService,
-    private routerService: Router,
     private loginService: LoginService
   ) {}
   ngOnInit(): void {
@@ -34,29 +32,6 @@ export class MenuComponent implements OnInit, OnDestroy {
 
   closeMenu() {
     this.menuService.closeMenu();
-  }
-
-  navigateMain(path: string, index: number) {
-    this.clearAllSelections();
-    this.mainElements[index].selected = true;
-    this.closeMenu();
-    this.routerService.navigate([path]);
-  }
-
-  navigateFooter(path: string, index: number) {
-    this.clearAllSelections();
-    this.footerElements[index].selected = true;
-    this.closeMenu();
-    this.routerService.navigate([path]);
-  }
-
-  clearAllSelections() {
-    this.mainElements.forEach((element) => {
-      element.selected = false;
-    });
-    this.footerElements.forEach((element) => {
-      element.selected = false;
-    });
   }
 
   handleLogoutButtonClick() {
